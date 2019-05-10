f91146056aa5a890f5939345228e53cb70a55d3b
Lukaskov <sabin6341@gmail.com>
2019-05-10 22:48:40 +0200

    update js

--- UI/js/javascriptfunction.js
+++ UI/js/javascriptfunction.js
@@ -1,17 +1,13 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
@@ -23,9 +19,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-
-
 function menu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -101,20 +94,16 @@ toggle between hiding and showing the dropdown content */
       }
     }
   }
-
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
   document.getElementById("main").style.marginLeft = "280px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
 }
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
-
 function showpassword() {
   var x = document.getElementById("pass");
   if (x.type === "password") {
