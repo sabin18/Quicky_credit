ba788a64df91d96d702ffc94f99983787c919a88
sabin18 <47029978+sabin18@users.noreply.github.com>
2019-05-13 11:36:40 +0200

    [ch-#165956711] update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(router);
-
+app.get('/', (req, res) => {
+    res.status(200).json({
+      status: 200,
+      message: 'Welcome to Quick_credit apply for loan Now!',
+    });
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Quick Credit server On....'));
 
